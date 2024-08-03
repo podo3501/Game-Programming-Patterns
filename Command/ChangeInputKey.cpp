1b45e195923b2bda21e46df9d9a275b34af3ae3d
podo3501 <nixie77@naver.com>
2024-08-03 23:44:35 +0900

    Command 패턴 작성중

--- Command/ChangeInputKey.cpp
+++ Command/ChangeInputKey.cpp
@@ -1,2 +1,38 @@
 #include "ChangeInputKey.h"
 
+void Jump() {};
+void FireGun() {};
+void SwapWeapon() {};
+
+void JumpCommand::Execute()
+{
+	Jump();
+}
+
+void FireCommand::Execute()
+{
+	FireGun();
+}
+
+void SwapCommand::Execute()
+{
+	SwapWeapon();
+}
+
+InputHandler::InputHandler() :
+	m_btnX{ std::make_unique<JumpCommand>() },
+	m_btnY{ std::make_unique<FireCommand>() },
+	m_btnZ{ std::make_unique<SwapCommand>() }
+{}
+
+bool InputHandler::isPressed(int n)
+{
+	return false;
+}
+
+void InputHandler::handleInput()
+{
+	if (isPressed('x')) m_btnX->Execute();
+	else if (isPressed('y')) m_btnY->Execute();
+	else if (isPressed('z')) m_btnZ->Execute();
+}
--- Command/ChangeInputKey.h
+++ Command/ChangeInputKey.h
@@ -1,1 +1,42 @@
 #pragma once
+
+#include <memory>
+
+class Command
+{
+public:
+	virtual ~Command() {}
+	virtual void Execute() = 0;
+};
+
+class JumpCommand : public Command
+{
+public:
+	virtual void Execute() override;
+};
+
+class FireCommand : public Command
+{
+public:
+	virtual void Execute() override;
+};
+
+class SwapCommand : public Command
+{
+public:
+	virtual void Execute() override;
+};
+
+class InputHandler
+{
+public:
+	InputHandler();
+
+	void handleInput();
+	bool isPressed(int n);
+
+private:
+	std::unique_ptr<Command> m_btnX;
+	std::unique_ptr<Command> m_btnY;
+	std::unique_ptr<Command> m_btnZ;
+};
